8b3fdb9fd59adf7ec089b90adef3344192e518da
Drago Vuckovic <drago.vuckovic@htecgroup.com>
2023-10-11 15:12:46 +0200

    kafka-streaming demo app built using gradle initial commit

---
+++ 6. Streaming/gradle/kafka-demo/src/test/java/co/vuckovic/streaming/kafkademo/KafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.vuckovic.streaming.kafkademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
